ed129e57f90eea7565b7b6b1649896dcadf54b67
Ody <95911512+Aygran@users.noreply.github.com>
2023-09-19 14:50:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver((entries) => {
+/* const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       console.log(entry)
       if (entry.isIntersecting) {
@@ -11,3 +11,5 @@ const observer = new IntersectionObserver((entries) => {
   
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el));
+
+*/
